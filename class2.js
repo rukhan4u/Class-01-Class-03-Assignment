87bfb11bbb9e5be99c49a41a1d05221a5f017a86
rukhan4u <163444214+rukhan4u@users.noreply.github.com>
2024-03-21 22:42:37 +0500

    Add files via upload
    Class-01-Class-03-Assignment For Hallow World and Using String

---
+++ class2.js
@@ -0,0 +1,24 @@
+// console.log("Hellow World")
+// Output: Hellow World
+// var name1 ="Rafiq Ullah Khan";
+// console.log(name1)
+//Output: Raiq Ullah Khan
+// let name1 = "Raiq Ullah Khan"
+// console.log(name1)
+// name1 = "2424"
+// console.log(name1)
+// Output: Rafiq Ullah Khan
+//         2424
+// let a = "Rafiq Ullah Khan"
+// let age = 44
+// console.log(a,age)
+// output: Rafiq Ullah Khan 44
+var name2 = "Raiq Ullah Khan";
+var age = "44";
+console.log(name2, age);
+// Output: Raiq Ullah Khan 44
+// const accountNum : number = 123456
+// console.log(accountNum)
+// Output 123456
+// let cellnumber : number = 3009207068
+// console.log(cellnumber) 
